d0240e62ae2eadfb55aace3772e66776f9a3032a
Heitor Rodrigues <heitor.vrb@gmail.com>
2025-09-04 05:35:57 -0300

    Add typography

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -25,9 +25,9 @@
     <div
         class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
         <main class="flex max-w-[335px] w-full flex-col-reverse lg:max-w-4xl lg:flex-row">
-            <div class="prose max-w-none">
+            <article class="prose dark:prose-invert max-w-none">
                 {!! $html !!}
-            </div>
+            </article>
         </main>
     </div>
 </body>
