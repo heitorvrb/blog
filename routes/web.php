89c85c2d05b25ef30d7ab63be1a8442c83dafb04
Heitor Rodrigues <heitor.vrb@gmail.com>
2025-09-06 21:21:55 -0300

    Delete unused default 'welcome' route

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,5 @@
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/welcome', function () {
-    return view('welcome');
-});
-
 Route::get('/', [PostController::class, 'index']);
 Route::get('/{slug}', [PostController::class, 'show']);
