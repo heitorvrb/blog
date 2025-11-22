ae9306a4a6c38ac8d2bea819d64c99da3be30e83
Heitor Rodrigues <heitor.vrb@gmail.com>
2025-11-22 13:31:56 -0300

    Envoy fix messaging

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,57 +1,49 @@
 @servers(['localhost' => '127.0.0.1'])
 
 @setup
-    $remote = 'origin';
-    $branch = 'master';
-    $projectDir = getenv('PROJECT_DIR');
-    $token = getenv('TELEGRAM_BOT_TOKEN');
-    $chat = getenv('TELEGRAM_CHAT_ID');
+$remote = 'origin';
+$branch = 'master';
+$projectDir = getenv('PROJECT_DIR');
+$bot = getenv('TELEGRAM_BOT_TOKEN');
+$chat = getenv('TELEGRAM_CHAT_ID');
 @endsetup
 
-
 @task('check')
-    bash -eu << 'EOF'
-    cd {{ $projectDir }}
+bash -eu << 'EOF'
+cd {{ $projectDir }}
 
-    OLD=$(git rev-parse HEAD)
-    git fetch {{ $remote }} {{ $branch }}
-    NEW=$(git rev-parse {{ $remote }}/{{ $branch }})
+OLD=$(git rev-parse HEAD)
+git fetch {{ $remote }} {{ $branch }}
+NEW=$(git rev-parse {{ $remote }}/{{ $branch }})
 
-    if [ "$OLD" != "$NEW" ]; then
-        echo "update"
-    else
-        echo "no-update"
-    fi
-    EOF
+if [ "$OLD" != "$NEW" ]; then
+    echo "update"
+else
+    echo "no-update"
+fi
+EOF
 @endtask
 
 @task('deploy')
-    bash -eu << 'EOF'
-    cd {{ $projectDir }}
-    git pull {{ $remote }} {{ $branch }}
+bash -eu << 'EOF'
+cd {{ $projectDir }}
 
-    composer install --no-dev --prefer-dist --no-interaction --optimize-autoloader
-    npm run build
-    php artisan optimize
-    EOF
+git pull {{ $remote }} {{ $branch }}
+composer install --no-dev --prefer-dist --no-interaction --optimize-autoloader
+npm run build
+php artisan optimize
+
+curl -fsS \
+    -o /dev/null \
+    -w '' \
+    "https://api.telegram.org/bot{{ $bot }}/sendMessage" \
+    --data-urlencode "chat_id={{ $chat }}" \
+    --data-urlencode "text=Deploy OK" \
+    > /dev/null 2>&1
+
 @endtask
 
-@after
-    $url = "https://api.telegram.org/bot{$token}/sendMessage";
-    $data = http_build_query([
-        'chat_id'=> $chat,
-        'text' => 'Deploy OK',
-    ]);
-
-    file_get_contents($url . '?' . $data);
-@endafter
 
 @error
-    $url = "https://api.telegram.org/bot{$token}/sendMessage";
-    $data = http_build_query([
-        'chat_id' => $chat,
-        'text' => '🟥 Error during deploy!',
-    ]);
-
-    file_get_contents($url . '?' . $data);
+    @telegram($bot, $chat, '🟥 Error during deploy!')
 @enderror
